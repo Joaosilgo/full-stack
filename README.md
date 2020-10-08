3ef055607f4fc1ff9c3076cb65ebc06b2842a960
João Gomes <joaosilgo96@gmail.com>
2020-10-08 05:36:20 +0100

    🧱

---
+++ README.md
@@ -0,0 +1,9 @@
+````bash
+echo "# full-stack" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Joaosilgo/full-stack.git
+git push -u origin main
+````                
---
+++ server.js
@@ -0,0 +1,29 @@
+
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.get('/api/customers', (req, res) => {
+  const customers = [
+    {id: 1, firstName: 'John', lastName: 'Doe'},
+    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
+    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
+  ];
+
+  res.json(customers)
+});
+
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+
+app.listen(port, () => console.log(`Server running on port at http://localhost:${port}`)) 
+
