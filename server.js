9371020c44042ca21fd001ca8086f335efeb4a20
João Gomes <joaosilgo96@gmail.com>
2020-10-08 19:03:29 +0100

    🧱

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ require('dotenv').config();
 //const dotenv = require('dotenv');
 //dotenv.config();
 
+
+
+process.env.NODE_ENV = 'production';
+
 const port =  process.env.PORT || 5000 ;
 
 console.log(process.env.NODE_ENV);
@@ -15,17 +19,20 @@ console.log(__dirname) ;
 
 // app.use(express.static('client/build'));
 
+
+
+
 app.use(express.static(path.join(__dirname, 'client/build')));
-
-
-
 
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   //app.use(express.static('client/build'));
-  app.use(express.static('client/build'));
+//app.use(express.static('client/build'));
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 
  
 }
@@ -34,15 +41,24 @@ if (process.env.NODE_ENV === 'production') {
 
 
 app.get('/', (req, res) => {
-  res.send('Go to /api/customer!')
+  res.send('Go to /api/customer!');
+  console.log('Go to /api/customer!');
 })
+
+
+app.get('/teste', (req, res) => {
+  
+  console.log('Teste!')
+  res.send('Teste')
+})
+
 
 
 app.get('/api/customers', (req, res) => {
   const customers = [
     {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
+    {id: 2, firstName: 'João', lastName: 'Gomes'},
+    {id: 3, firstName: 'João', lastName: 'Silva'},
   ];
 
   res.json(customers)
@@ -50,11 +66,20 @@ app.get('/api/customers', (req, res) => {
 
 //const port = process.env.PORT || 5000 ;
 
-
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  //app.use(express.static('client/build'));
+//app.use(express.static('client/build'));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
+
+
+
+ 
+}
+
 
 /*
 app.get('*', (req, res) => {
